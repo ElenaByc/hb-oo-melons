7f840cd2c03c9a1336f6bb69fd82e619be467db6
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-04-13 22:35:12 -0700

    feat: part1 - create abstract parent class and refactor two child classes

--- melons.py
+++ melons.py
@@ -1,17 +1,23 @@
 """Classes for melon orders."""
 
 
-class DomesticMelonOrder:
-    """A melon order within the USA."""
+class AbstractMelonOrder:
+    """An abstract base class that other Melon Orders inherit from."""
 
-    def __init__(self, species, qty):
+    # the following 2 lines can be included, but are not necessary because
+    # AbstractMelonOrder should never be instantiated directly
+
+    # order_type = None
+    # tax = 0
+
+    def __init__(self, species, qty, order_type, tax):
         """Initialize melon order attributes."""
 
         self.species = species
         self.qty = qty
+        self.order_type = order_type
+        self.tax = tax
         self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
 
     def get_total(self):
         """Calculate price, including tax."""
@@ -25,6 +31,21 @@ class DomesticMelonOrder:
         """Record the fact than an order has been shipped."""
 
         self.shipped = True
+
+
+class DomesticMelonOrder:
+    """A melon order within the USA."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+
+    #     self.species = species
+    #     self.qty = qty
+    #     self.shipped = False
+    #     self.order_type = "domestic"
+    #     self.tax = 0.08
+
+        super().__init__(species, qty, "domestic", 0.08)
 
 
 class InternationalMelonOrder:
@@ -33,25 +54,15 @@ class InternationalMelonOrder:
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
 
-        self.species = species
-        self.qty = qty
+        # self.species = species
+        # self.qty = qty
+        # self.country_code = country_code
+        # self.shipped = False
+        # self.order_type = "international"
+        # self.tax = 0.17
+
+        super().__init__(species, qty, "international", 0.17)
         self.country_code = country_code
-        self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
 
     def get_country_code(self):
         """Return the country code."""
